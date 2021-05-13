51475afda1cca0948edca5ed4e0db37355cfd447
Rachel Nascimento <rachelle.nascimento@gmail.com>
2021-05-13 18:18:59 +0200

    watch list - back part done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,9 +3,4 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :comment, length: { minimum: 6 }
   validates_uniqueness_of :movie, :scope => [:list]
-  # validates :movie_id, presence: true
-  # validates :user_id, presence: true
-  # validates_uniqueness_of :id, :scope => [:movie_id, :list_id]
-  # validates :id, uniqueness: { scope: :movie, :list }
-  # uniqueness: { scope: :movie, list: }
 end
